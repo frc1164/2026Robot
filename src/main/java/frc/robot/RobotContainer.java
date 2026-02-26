81933b37b174ffc64700b01f851dda7099f53469
ExtremelyProfessionalUsername <liamkiser08@gmail.com>
2026-02-25 17:16:12 -0700

    moved climb into the right directory

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,11 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.Newton;
 
-import Climber.Climb;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Climber.Climb;
 import frc.robot.Swerve.SwerveJoystickCmd;
 import frc.robot.Swerve.SwerveSubsystem;
 import edu.wpi.first.wpilibj.Compressor;
